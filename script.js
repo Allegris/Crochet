b107eb95b88c1bbfe5a2db3e55b76f8002d7594e
Astrid Christiansen <astridpigen2@gmail.com>
2017-11-30 20:48:56 +0100

    3 rotating boxes

--- script.js
+++ script.js
@@ -10,12 +10,25 @@ function init() {
 
 	scene = new THREE.Scene();
 
-	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+	
+	geometry = new THREE.BoxGeometry( 0.2, 0.5, 0.10);//Size of box
 	material = new THREE.MeshNormalMaterial();
-
+	
+	//Mesh1
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
 
+	//Mesh 2
+	mesh2 = new THREE.Mesh( geometry, material );
+	scene.add( mesh2 );
+	mesh2.position.set(0.2,0,0);
+	
+	//Mesh 3
+	mesh3 = new THREE.Mesh( geometry, material );
+	scene.add( mesh3 );
+	mesh3.position.set(-0.2,0,0);
+
+	
 	renderer = new THREE.WebGLRenderer( { antialias: true } );
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
@@ -29,6 +42,12 @@ function animate() {
 	mesh.rotation.x += 0.01;
 	mesh.rotation.y += 0.02;
 
+	mesh2.rotation.x += 0.01;
+	mesh2.rotation.y += 0.02;
+	
+	mesh3.rotation.x += 0.01;
+	mesh3.rotation.y += 0.02;
+	
 	renderer.render( scene, camera );
 
 }
